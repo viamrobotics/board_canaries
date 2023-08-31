3f85e91c94d5c189a5fc269b2934d5a65840ed3f
Alan Davidson <alan@viam.com>
2023-08-31 18:19:33 -0400

    more work

--- analyze_logs.py
+++ analyze_logs.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
+import fileinput # For accessing stdin
 
 # TODO: remember to add slack_sdk to requirements.txt when that file exists
-
 import slack_sdk
 
 import slack_reporter_config as config
+
 
 def report_errors(output):
     client = slack_sdk.WebClient(token=config.auth_token)
@@ -16,6 +17,16 @@ def report_errors(output):
     result = client.chat_postMessage(
             channel=config.channel, text=text)
     # If we get a result, things worked. Failure raises exceptions instead.
+    # Currently, we ignore any errors. How do we report that we're unable to
+    # report stuff!?
 
-vals = ["Here's the first line", "Here's the second line. It's longer"]
-report_errors(vals)
+
+def tests_succeeded(contents):
+    return contents[-2:] == ["Success!", "done running tests!"]
+
+
+
+if __name__ == "__main__":
+    contents = [line for line in fileinput.input()]
+    if not tests_succeeded(contents):
+        report_errors(contents)
