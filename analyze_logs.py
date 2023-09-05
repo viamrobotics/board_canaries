7b34a2289b756caa4ff4941b7af1665c52aa97b3
Alan Davidson <alan@viam.com>
2023-09-05 12:15:41 -0400

    also write to slack if you can't find the daily entry

--- analyze_logs.py
+++ analyze_logs.py
@@ -7,6 +7,16 @@ import slack_sdk
 import slack_reporter_config as config
 
 
+def report_no_output():
+    client = slack_sdk.WebClient(token=config.auth_token)
+    text = ("The canary tests on the {} board had no recent output!"
+            .format(config.board_name))
+    result = client.chat_postMessage(channel=config.channel, text=text)
+    # If we get a result, things worked. Failure raises exceptions instead.
+    # Currently, we ignore any errors. How do we report that we're unable to
+    # report stuff!?
+
+
 def report_errors(output):
     client = slack_sdk.WebClient(token=config.auth_token)
     text = ("The canary tests on the {} board have failed. Recent output is:"
@@ -15,9 +25,8 @@ def report_errors(output):
 
     result = client.files_upload_v2(
         channel=config.channel, content=file_contents, initial_comment=text)
-    # If we get a result, things worked. Failure raises exceptions instead.
-    # Currently, we ignore any errors. How do we report that we're unable to
-    # report stuff!?
+    # Again, if things fail we'll raise an exception here, but there is no
+    # obvious resolution for that.
 
 
 def tests_succeeded(contents):
@@ -27,4 +36,7 @@ def tests_succeeded(contents):
 if __name__ == "__main__":
     contents = [line for line in fileinput.input()]
     if not tests_succeeded(contents):
-        report_errors(contents)
+        if contents == []:
+            report_no_output()
+        else:
+            report_errors(contents)
