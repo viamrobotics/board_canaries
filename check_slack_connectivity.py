d072d0f711c856d0982d55e9c24340d0666aa284
Alan Davidson <alan@viam.com>
2023-09-05 16:28:08 -0400

    Add explanatory comment

--- check_slack_connectivity.py
+++ check_slack_connectivity.py
@@ -4,6 +4,11 @@ import slack_sdk
 import slack_reporter_config as config
 
 
+# This is simultaneously:
+# 1) a way to demonstrate that you put the correct secrets into
+#     slack_reporter_config.py, and
+# 2) an announcement to the Slack channel that there's a new board running
+#     canary tests.
 if __name__ == "__main__":
     client = slack_sdk.WebClient(token=config.auth_token)
     text = ("Canary tests on the {} board should be able to report to Slack!"
