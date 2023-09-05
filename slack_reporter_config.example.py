06067b98671246fde4ca6b24cef9f199acab9948
Alan Davidson <alan@viam.com>
2023-09-05 16:43:18 -0400

    PR feedback: include instructions of how to find channel ID

--- slack_reporter_config.example.py
+++ slack_reporter_config.example.py
@@ -1,4 +1,7 @@
 auth_token = "xoxb-secret-goes-here" # Should start with "xoxb-"
 board_name = "example board"
 
-channel_id = "C12345ABCDE" # The channel ID, *not* the channel name!
+# This is the channel ID, *not* the channel name! To find the ID, go to the
+# channel, click on the name at the top to open the channel options, and
+# scroll down to the bottom.
+channel_id = "C12345ABCDE"
