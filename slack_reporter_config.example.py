f9e8a0c7698d2f9fd2b2491841122e100cfb9dd9
Alan Davidson <alan@viam.com>
2023-09-05 15:50:34 -0400

    Add clarifying comment

--- slack_reporter_config.example.py
+++ slack_reporter_config.example.py
@@ -1,4 +1,4 @@
 auth_token = "xoxb-secret-goes-here" # Should start with "xoxb-"
 board_name = "example board"
 
-channel_id = "C12345ABCDE"
+channel_id = "C12345ABCDE" # The channel ID, *not* the channel name!
