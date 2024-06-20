9001c75e1e38cf3b097d6302ee1c1a12153786c9
Alan Davidson <alan@viam.com>
2024-06-20 10:30:15 -0400

    specify the private key is in the local directory

--- monitor_other_boards.py
+++ monitor_other_boards.py
@@ -18,12 +18,13 @@ all_boards = (("odroid@odroid",         "odroid-C4"),
 
 
 def ensure_board_is_online(ssh_target, name):
+    print(f"now checking the {name} board...")
     today = datetime.today().strftime("%Y-%m-%d")
     # This command SSHes into the target machine and counts how many times the
     # canary analysis logfile contains a line with today's date and nothing
     # else. If the analysis has run, there should be exactly 1 of these lines.
     command = f"cat /var/log/canary_tests.log | grep '^{today}$' | wc -l"
-    subprocess_command = f"ssh '{ssh_target}' \"{command}\""
+    subprocess_command = f"ssh -i board_canary.priv '{ssh_target}' \"{command}\""
     try:
         subprocess_result = subprocess.run(
             subprocess_command, timeout=60, shell=True, capture_output=True)
@@ -32,7 +33,7 @@ def ensure_board_is_online(ssh_target, name):
         return
     if subprocess_result.returncode != 0:
         slack_reporter.report_message(f"Cannot read from the {name} board!")
-    if subprocess_result.stdout != b"1\n"):
+    if subprocess_result.stdout != b"1\n":
         slack_reporter.report_message(f"Unexpected logs on the {name} board!")
 
 
