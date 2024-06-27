6456c2587453e240610218e1f2891a4f4010f8c0
Alan Davidson <alan@viam.com>
2024-06-27 11:01:38 -0400

    Revert "run the monitor in the cloud, not on the boards"
    This reverts commit 9a5c3f385986c89f4a50280daa25fb23ef4b0956.

--- canary_tests.py
+++ canary_tests.py
@@ -128,6 +128,22 @@ class PinTests(unittest.IsolatedAsyncioTestCase):
             if should_stop.is_set():
                 return
 
+class PingMonitorTest(unittest.TestCase):
+    def test_monitor_is_pingable(self):
+        """
+        One board canary is designated the "canary monitor," and makes sure
+        that all other canaries are online, reachable, and have run their
+        analysis in the past day. There is a different canary that's in charge
+        of making sure the canary monitor is online, and that's what this test
+        is about.
+        """
+        if config.ssh_monitor is None:
+            return  # No monitor to ping
+
+        subprocess_result = subprocess.run(
+            ["ping", "-c", "1", config.ssh_monitor], timeout=30)
+        self.assertEqual(subprocess_result.returncode, 0)
+
 
 if __name__ == "__main__":
     unittest.main()
