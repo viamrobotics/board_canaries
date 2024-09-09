207d4db49bf3b10d610371656769c79bea65f74b
oliviamiller <106617921+oliviamiller@users.noreply.github.com>
2024-09-09 14:43:27 -0400

    use address instead of conf.address

--- canary_tests.py
+++ canary_tests.py
@@ -29,7 +29,7 @@ async def connect(address, opts):
                        shell=True)
         print("Connection error during canary tests. Retrying...")
         time.sleep(5)
-        robot = await RobotClient.at_address(conf.address, opts)
+        robot = await RobotClient.at_address(address, opts)
     return robot
 
 
